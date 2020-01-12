5fe89cd93b76170f17a59931e05b4a7b487f2150
reg77777 <junpei1216@outlook.jp>
2020-01-13 03:24:38 +0900

    [Add]modintとグラフを追加

--- snippet/alg/modcomb.cpp
+++ snippet/alg/modcomb.cpp
@@ -1,39 +1,35 @@
 //O(N)
 #include <iostream>
 #include <vector>
+#include <cstdint>
 
-int main(){
 
-  struct Modcomb{
-    int mod; 
-    std::vector<long long int>fac; 
-    std::vector<long long int>finv; 
-    std::vector<long long int>inv;
+template<std::uint_fast64_t mod>
+struct Modcomb{
+  using u64=uint_fast64_t;
+  std::vector<u64>fac; 
+  std::vector<u64>finv; 
+  std::vector<u64>inv;
 
-    Modcomb(int max,int mod){
-      this->mod=mod;
-      fac.resize(max+1,0);
-      finv.resize(max+1,0);
-      inv.resize(max+1,0);
-      fac[0]=fac[1]=1;
-      finv[0]=finv[1]=1;
-      inv[1]=1;
-      for(int i=2;i<=max;i++){
-        fac[i]=(fac[i-1]*i%mod)%mod;
-        inv[i]=mod-inv[mod%i]*(mod/i)%mod;
-        finv[i]=finv[i-1]*inv[i]%mod;
-      }
+  Modcomb(u64 max){
+    this->mod=mod;
+    fac.resize(max+1,0);
+    finv.resize(max+1,0);
+    inv.resize(max+1,0);
+    fac[0]=fac[1]=1;
+    finv[0]=finv[1]=1;
+    inv[1]=1;
+    for(int i=2;i<=max;i++){
+      fac[i]=(fac[i-1]*i%mod)%mod;
+      inv[i]=mod-inv[mod%i]*(mod/i)%mod;
+      finv[i]=finv[i-1]*inv[i]%mod;
     }
+  }
 
-    //nCr=n!/r!(n-r)!
-    long long int slove(int n,int r){
-      if(n<r)return 0;
-      if(n<0||r<0)return 0;
-      return fac[n]*(finv[r]*finv[n-r]%mod)%mod;
-    }
-  }modcomb(5100,1000000007);
-
-  std::cout<<modcomb.slove(5,2);
-
-  return 0;
-}
+  //nCr=n!/r!(n-r)!
+  u64 slove(u64 n,u64 r){
+    if(n<r)return 0;
+    if(n<0||r<0)return 0;
+    return fac[n]*(finv[r]*finv[n-r]%mod)%mod;
+  }
+};
