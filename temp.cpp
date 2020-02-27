b19040ecd9d78e0e0fec711ed8128387585fa77f
reg77777 <junpei1216@outlook.jp>
2020-02-27 17:29:27 +0900

    [Add] コンテスト用スクリプト追加

---
+++ temp.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+
+using namespace std; //ごめんなさい
+using ll=long long;
+using ull=unsigned long long;
+using pii=pair<int,int>;
+
+#define INF LONG_MAX
+#define MOD 1000000007
+#define rng(a) a.begin(),a.end()
+#define rrng(a) a.end(),a.begin()
+#define rep(i,N) for(int i=0;i<N;i++)
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  return 0;
+}
