2843c4b93580d5d5541d504fdd4082b65f73f423
reg77777 <junpei1216@outlook.jp>
2020-03-01 02:28:02 +0900

    [Update] 整理&整理に伴いスクリプト修正

--- snippet/build_vimsnippet.sh
+++ snippet/build_vimsnippet.sh
@@ -3,14 +3,22 @@
 rm cpp.snip
 touch cpp.snip
 
-for dir in alg ds
+#データ構造のスニペット作成
+for var in ds/*.cpp
 do
-  for var in ${dir}/*.cpp
-  do
-    title=`echo "${var}" | sed -e 's/'${dir}'\///'`
-    v=`cat ${var} | nkf -Ww`
-    V=`echo "${v}" | sed  -e '/^#/d' -e '/^using namespace std/d' -e '/^\/\//d' -e 's/^/\t/'`
-    V="snippet ${title} ${V}"
-    echo "${V}" >> cpp.snip
-  done
+  title=`echo "${var}" | sed -e 's/ds\///'`
+  v=`cat ${var} | nkf -Ww`
+  V=`echo "${v}" | sed  -e '/^#/d' -e '/^using namespace std/d' -e '/^\/\//d' -e 's/^/\t/'`
+  V="snippet ${title} ${V}"
+  echo "${V}" >> cpp.snip
+done
+
+#アルゴリズムのスニペット作成
+for var in alg/*/*.cpp
+do
+  title=`echo "${var}" | sed -e 's/alg\///'`
+  v=`cat ${var} | nkf -Ww`
+  V=`echo "${v}" | sed  -e '/^#/d' -e '/^using namespace std/d' -e '/^\/\//d' -e 's/^/\t/'`
+  V="snippet ${title} ${V}"
+  echo "${V}" >> cpp.snip
 done
