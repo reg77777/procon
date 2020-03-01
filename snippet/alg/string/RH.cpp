1b3ff4f2dce5a81b2fb7e08e44e73352ea831edb
reg77777 <junpei1216@outlook.jp>
2020-03-01 18:03:28 +0900

    [Fix] ローリングハッシュのmodを複数にした

--- snippet/alg/string/RH.cpp
+++ snippet/alg/string/RH.cpp
@@ -2,34 +2,45 @@
 #include <string>
 #include <cstdint>
 
-using namespace std; //„Åî„ÇÅ„Çì„Å™„Åï„ÅÑ
+using namespace std; //Ç≤ÇﬂÇÒÇ»Ç≥Ç¢
 
 vector<int> RH(string S,string T){
   int SN=S.size();
   int TN=T.size();
-  uint_fast64_t b=1e6+7; 
-  uint_fast64_t mod=1e9+7; 
-  uint_fast64_t s=0;
-  uint_fast64_t t=0;
-  uint_fast64_t a=1;
+  int_fast64_t b=9973; 
+  vector<int_fast64_t>mod={1000000007LL,1000000009LL,1000000021LL}; 
+  int M=mod.size();
+  vector<int_fast64_t>s(M,0);
+  vector<int_fast64_t>t(M,0);
+  vector<int_fast64_t>a(M,1);
   for(int i=0;i<TN;i++){
-    a*=b;
-    a%=mod;
+    for(int j=0;j<M;j++){
+      a[j]=(a[j]*b)%mod[j];
+    }
   }
   for(int i=0;i<TN;i++){
-    s=s*b+S[i];
-    s%=mod;
+    for(int j=0;j<M;j++){
+      s[j]=((s[j]*b)%mod[j]+S[i])%mod[j];
+    }
   }
   for(int i=0;i<TN;i++){
-    t=t*b+T[i];
-    t%=mod;
+    for(int j=0;j<M;j++){
+      t[j]=((t[j]*b)%mod[j]+T[i])%mod[j];
+    }
   }
   vector<int>res;
-  if(s==t)res.push_back(0);
+  bool ok=true;
+  for(int i=0;i<M;i++)if(s[i]!=t[i])ok=false;
+  if(ok)res.push_back(0);
   for(int i=0;i+TN<SN;i++){
-    s=s*b-S[i]*a+S[i+TN];
-    s%=mod;
-    if(s==t)res.push_back(i+1);
+    ok=true;
+    for(int j=0;j<M;j++){
+      s[j]=((s[j]*b)%mod[j]-(S[i]*a[j])%mod[j])%mod[j];
+      if(s[j]<0)s[j]+=mod[j];
+      s[j]=(s[j]+S[i+TN])%mod[j];
+      if(s[j]!=t[j])ok=false;
+    }
+    if(ok)res.push_back(i+1);
   }
   return res;
 }
