55d82c2bef841211c2d9a5737ad95ecefbaadb65
reg77777 <junpei1216@outlook.jp>
2020-03-02 02:27:46 +0900

    [Add] BIT追加

---
+++ snippet/ds/bit.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std; //ごめんなさい
+
+//0-index
+struct BIT{
+  vector<int>node;
+  int sz;
+  BIT(int sz):sz(sz){
+    node.resize(sz,0);
+  }
+  int sum(int i){
+    int res=0;
+    for(i--;i>=0;i=(i&(i+1))-1)res+=node[i];
+    return res;
+  }
+  void add(int i,int x){
+    for(;i<sz;i|=i+1)node[i]+=x;
+  }
+  int sum(int l,int r){
+    return sum(r)-sum(l);
+  }
+};
