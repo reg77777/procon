7ede95dcd8f58db0ffbbb5c32720e73f5230295d
reg77777 <junpei1216@outlook.jp>
2020-03-04 22:34:49 +0900

    [Fix] Union Findの連結成分のサイズが正しく出ないバグを修正

--- snippet/ds/uf.cpp
+++ snippet/ds/uf.cpp
@@ -8,14 +8,14 @@ struct UF{
   vector<int>p; //p[i]=ノードiの親
   vector<int>r; //rank[i]=ノードiのランク
   UF(int sz){
-    r.resize(sz,0);
+    r.resize(sz,1);
     p.resize(sz);
     for(int i=0;i<sz;i++)p[i]=i;
   }
   void u(int x,int y){ //1
+    if(isu(x,y))return;
     x=root(x);
     y=root(y);
-    if(r[x]==r[y])r[x]++;
     if(r[x]>r[y])p[y]=x,r[x]+=r[y];
     else p[x]=y,r[y]+=r[x];
   }
