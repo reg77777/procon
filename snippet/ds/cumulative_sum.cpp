281fd746093979f56f19ed082a76be321b555ba9
reg77777 <junpei1216@outlook.jp>
2020-03-07 20:41:17 +0900

    [Add] 累積和追加

---
+++ snippet/ds/cumulative_sum.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <cstdint>
+
+using namespace std; //ごめんなさい
+
+struct CSum{
+  vector<int_fast64_t>node;
+  template<typename T>
+  CSum(vector<T>v){
+    node.resize(v.size(),0);
+    for(int i=0;i<v.size();i++)node[i+1]=node[i]+v[i];
+  }
+  int_fast64_t sum(int x){
+    return node[x];
+  }
+  int_fast64_t sum(int l,int r){
+    return node[r]-node[l];
+  }
+};
