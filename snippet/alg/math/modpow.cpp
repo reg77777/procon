2eb0cd185c16029a643357a52d0460f34c431241
reg77777 <junpei1216@outlook.jp>
2020-03-10 01:53:31 +0900

    [Fix] modpowバグってた(は？)

--- snippet/alg/math/modpow.cpp
+++ snippet/alg/math/modpow.cpp
@@ -5,10 +5,10 @@ using namespace std; //ごめんなさい
 template<uint_fast64_t m>
 int_fast64_t mpow(int_fast64_t x,int_fast64_t n){
   using i64=int_fast64_t;
-  if(x==0)return 1;
-  if(x%2)return ((n%m)*(mpow<m>(n,x-1)%m))%m;
+  if(n==0)return 1;
+  if(n%2)return ((x%m)*(mpow<m>(x,n-1)%m))%m;
   else {
-    i64 half=mpow<m>(n,x/2)%m;
+    i64 half=mpow<m>(x,n/2)%m;
     return (half*half)%m;
   }
 }
