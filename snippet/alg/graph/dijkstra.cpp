f5c04f0717539bda56f2b6db781944e7ad58a63b
reg77777 <junpei1216@outlook.jp>
2020-03-29 13:17:07 +0900

    [Update] ダイクストラをきれいにした

--- snippet/alg/graph/dijkstra.cpp
+++ snippet/alg/graph/dijkstra.cpp
@@ -1,29 +1,29 @@
-#include <vector>
-#include <queue>
-#include <cstdint>
-
+#include<bits/stdc++.h>
 using namespace std;
 
+template<typename T>
 struct E{
   int to;
-  int_fast64_t cost;
+  T cost;
 };
 
-vector<int_fast64_t>dijkstra(vector<vector<E>>g,int s,int_fast64_t inf){
-  vector<int_fast64_t>d(g.size(),inf);
-  using T=pair<int_fast64_t,int>;
-  using C=vector<T>;
-  using Cmp=greater<T>;
-  priority_queue<T,C,Cmp>q; 
+template<typename T>
+vector<T>dijkstra(vector<vector<E<T>>>g,int s){
+  T inf=numeric_limits<T>::max();
+  vector<T>d(g.size(),inf);
+  using Type=pair<T,int>;
+  using Container=vector<Type>;
+  using Cmp=greater<Type>;
+  priority_queue<Type,Container,Cmp>q; 
   d[s]=0;
   q.push({d[s],s});
   while(!q.empty()){
-    int_fast64_t cost=q.top().first;
+    T cost=q.top().first;
     int from=q.top().second;
     q.pop();
     if(d[from]!=cost)continue;
     for(auto e:g[from]){
-      int_fast64_t next_cost=e.cost+cost;
+      T next_cost=e.cost+cost;
       if(d[e.to]>next_cost){
         d[e.to]=next_cost;
         q.push({d[e.to],e.to});
