ba39ec17535e534b7493386ec8b8fd755fa51165
reg77777 <junpei1216@outlook.jp>
2020-03-29 12:29:51 +0900

    [FIX] LIS修正

--- snippet/alg/dp/lis.cpp
+++ snippet/alg/dp/lis.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 template<typename T,T inf>
-int lis(vector<T>v){
+int lis(vector<T>&v){
   int N=v.size();
   vector<T>dp(N,inf);
   for(int i=0;i<N;i++)*lower_bound(dp.begin(),dp.end(),v[i])=v[i];
