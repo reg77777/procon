cb8b855306d44fedf3660707cc3a25579ec0a40b
reg77777 <junpei1216@outlook.jp>
2020-03-29 13:03:32 +0900

    [Update] numeric_limitsを使った

--- snippet/alg/graph/bellman_ford.cpp
+++ snippet/alg/graph/bellman_ford.cpp
@@ -1,10 +1,15 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-struct E{int to,from,cost;};
+template<typename T>
+struct E{
+  int from,to;
+  T cost;
+};
 
-template<typename T,T inf>
-vector<T>bellman_ford(vector<E>&g,int s,int sz){
+template<typename T>
+vector<T>bellman_ford(vector<E<T>>&g,int s,int sz){
+  T inf=numeric_limits<T>::max();
   vector<T>d(sz,inf);
   d[s]=0;
   for(int i=0;i<sz-1;i++)for(auto e:g)if(d[e.from]!=inf)d[e.to]=min(d[e.to],d[e.from]+e.cost);
